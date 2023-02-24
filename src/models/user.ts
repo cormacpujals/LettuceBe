97192330269819c0e79a0c2cbf9cf74acbeed2ce
Tony Pujals <tonypujals@google.com>
2023-02-24 03:51:28 -0800

    Mongodb (#9)
    * add mongodb driver
    
    * add dotenv
    
    * replace in-memory db mock with mongodb

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,13 @@
+import {ObjectId} from "mongodb";
+
 import {Inventory} from "./inventory";
 
-// MOCK - delete later and import from MongoDB
-// import { ObjectId } from "mongodb";
-export type ObjectId = string;
-
 export class User {
-  _id: ObjectId = "";
+  _id?: ObjectId;
   name: string;
   inventory: Inventory;
 
   constructor(name: string, inventory?: Inventory) {
-    this._id = "";
     this.name = name;
     this.inventory = inventory || new Inventory();
   }
