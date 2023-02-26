2c0f5240f8b8407b4fa98398d842676b8d068cb2
Cormac Pujals <110591827+cormacpujals@users.noreply.github.com>
2023-02-26 13:06:35 -0800

    closes #15 gets user page (#16)

--- src/app/user/page.tsx
+++ src/app/user/page.tsx
@@ -1,3 +1,23 @@
 import {MagicUserId} from "../../../scripts/seed";
+import {Inventory} from "../../models/inventory";
 
 // TODO use MagicUserId hardcoded for now
+
+
+async function getUser() {
+  const res = await fetch('http://localhost:3000/api/user');
+  if(!res.ok) {
+    console.log(res);
+  }
+  return res.json();
+}
+
+export default async function User() {
+  const user : {name: string, inventory: Inventory} = await getUser();
+  console.log(user)
+  return (
+      <div>
+        {user.name}
+      </div>
+  )
+}
